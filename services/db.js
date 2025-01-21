b37dcbaeb1fbc60533d20852aa06c7de0599c88e
Siddiqui Amaan <90834290+Amaansiddiq@users.noreply.github.com>
2025-01-22 01:58:31 +0530

    Update db.js

--- services/db.js
+++ services/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Amaan9869',
+  password: 'xxxx',
   database: 'product_db'
 });
   
